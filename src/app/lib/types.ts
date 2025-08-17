8ca8b23bafe159bb7f702e9f870eb792d7511b45
Tyler Pohl <tylercurtispohl@gmail.com>
2025-08-17 15:38:57 -0700

    Initial fixes * set up database * remove unnecessary console logs * add Advocate type * reset search input when reset search button is clicked * search advocate full name rather than first and last separately * fix implicit any type in search input's onChange function * add keys to components produced by map functions

---
+++ src/app/lib/types.ts
@@ -0,0 +1,10 @@
+export type Advocate = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  city: string;
+  degree: string;
+  specialties: string[];
+  yearsOfExperience: number;
+  phoneNumber: number;
+};
